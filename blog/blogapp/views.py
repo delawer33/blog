05e8ee261e5e20e0e4fd4c9188cbbda3d0ec7286
delawer <shukry.aliev@mail.ru>
2024-01-11 23:05:02 +0300

    added blogapp.css, made displaying the list of posts

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blogapp/post/list.html',
+                  {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id,
+                             status=Post.Status.Published)
+    return render(request,
+                  'blogapp/post/detail.html',
+                  {'post': post})
