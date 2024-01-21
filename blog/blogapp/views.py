77d353b2bf826500821596e534ccdd98e8c8dede
unknown <shukry.aliev@mail.ru>
2024-01-21 20:25:03 +0300

    added comments

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -32,6 +34,24 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blogapp/post/detail.html',
-                  {'post': post})
+                  {'post': post, 'comments': comments, 'form':form})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blogapp/post/comment.html',
+                  {'post':post, 'form':form, 'comment':comment})
