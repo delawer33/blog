e0886d8a154494c630a3621a9702b52fbbb880d7
unknown <shukry.aliev@mail.ru>
2024-01-23 15:47:32 +0300

    similar posts recommendation

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.http import require_POST
 from .models import Post, Comment
 from .forms import CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -43,9 +44,16 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = (Post.published.filter(tags__in=post_tags_ids)
+                     .exclude(id=post.id))
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                        .order_by('-same_tags', 'publish')[:4]
+
     return render(request,
                   'blogapp/post/detail.html',
-                  {'post': post, 'comments': comments, 'form':form})
+                  {'post': post, 'comments': comments, 'form':form, 'similar_posts': similar_posts})
 
 @require_POST
 def post_comment(request, post_id):
