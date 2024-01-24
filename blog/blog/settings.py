1c2b81694ee4bf98cd0656904761c8ebf904f550
unknown <shukry.aliev@mail.ru>
2024-01-24 15:28:44 +0300

    markdown

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogapp.apps.BlogappConfig',
     'taggit',
+
 ]
 
 MIDDLEWARE = [
--- blog/blogapp/templatetags/blogapp_tags.py
+++ blog/blogapp/templatetags/blogapp_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db.models import Count
 from ..models import Post
+import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -9,11 +11,15 @@ def total_posts():
     return Post.published.count()
 
 @register.simple_tag
-def get_most_сommented_posts(count=5):
-    return (Post.publish.annotate(total_comments=Count('comments'))
+def get_most_commented_posts(count=5):
+    return (Post.published.annotate(total_comments=Count('comments'))
             .order_by('-total_comments'))[:count]
 
 @register.inclusion_tag('blogapp/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts':latest_posts}
+    return {'latest_posts': latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
